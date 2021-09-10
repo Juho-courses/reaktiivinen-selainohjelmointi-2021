6b0ca623c63d6863507d3ee91fedf83b4fd39cba
Juho Salli <juho.salli@gmail.com>
2021-09-10 14:38:07 +0300

    Lisää 07 koodit

---
+++ 07-loops-usestate/loops/src/components/SongList.js
@@ -0,0 +1,27 @@
+import Song from "./Song";
+import { useState } from "react";
+
+const SongList = () => {
+  const [songs] = useState([
+    { song: "biisi 1", artist: "esittäjä 1", year: "2001" },
+    { song: "biisi 2", artist: "esittäjä 2", year: "2002" },
+    { song: "biisi 3", artist: "esittäjä 3", year: "2003" },
+  ]);
+
+  return (
+    <div>
+      {songs.map(({ song, artist, year }, index) => {
+        return (
+          <Song
+            key={song + "-" + index}
+            song={song}
+            artist={artist}
+            year={year}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default SongList;
